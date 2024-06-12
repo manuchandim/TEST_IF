f17ff2923db7f80a868631154d71ef95beda4c0f
Manula Chandupa <manuchandim22@gmail.com>
2024-06-12 11:25:03 +0530

    first commit

---
+++ IF/IfClause.java
@@ -0,0 +1,22 @@
+
+public class IfClause {
+
+    public static void main(String[] args) {
+
+        Boolean x = false;
+
+        if (!x) {
+            System.out.println("done");
+        }
+
+        else{
+            System.out.println("error");
+        }
+
+
+        
+        
+    }
+
+
+}
